8422a1b4fc8a13a0fcb30f33526c671c8e01779f
Vinicius Sarmento <vinicius.siqueira@squadra.com.br>
2018-06-04 09:43:07 -0300

    ADD - Novo curso de Angular 2 e 4

--- aula-003-interface/run.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var animal_dao_1 = require("./animal-dao");
-var animal_1 = require("../aula-002-classes/animal");
-var dao = new animal_dao_1.AnimalDao();
-var animal = new animal_1.Animal('Rex');
-dao.insert(animal);
