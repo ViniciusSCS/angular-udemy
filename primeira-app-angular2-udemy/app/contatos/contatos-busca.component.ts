c80f281f9b57a21741b12a95a76270875804971a
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2018-06-13 09:06:17 -0300

    UPDATE -Funcionslidade de busca de contatos.

--- primeira-app-angular2-udemy/app/contatos/contatos-busca.component.ts
+++ primeira-app-angular2-udemy/app/contatos/contatos-busca.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Contatos } from './contatos';
+import { ContatosService } from './contatos.service';
 
 @Component({
     moduleId: module.id,
@@ -7,11 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ContatosBusca implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    contatos: Observable<Contatos[]>;
+    private termosDaBusca: Subject<string> = new Subject<string>();
+
+    constructor(
+        private contatosService: ContatosService
+    ) { }
+
+    ngOnInit(): void {
+        this.contatos = this.termosDaBusca
+            .switchMap(termo => {
+                console.log('Fez a busca: ', termo);
+                return termo ? this.contatosService.search(termo) : Observable.of<Contatos[]>([]);
+            });
+        
+            this.contatos.subscribe((contatos: Contatos[]) => {
+                console.log('retornou do servidor: ', contatos)
+            })
+    }
 
     search(termo: string): void {
         console.log(termo);
+        this.termosDaBusca.next(termo);
     }
 }
